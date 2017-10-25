61f7c0706c723e823c9f3fa99a2e977fe9ff48d0
Ardian Shala <ardian.sh@gmx.de>
2017-10-25 14:43:37 +0200

    added modules

--- app/app.js
+++ app/app.js
@@ -1,13 +1,8 @@
-angular.module("app", ["ngRoute", "ngMaterial"]);
+angular.module("app", ["ngRoute", "ngAnimate", "ngAria", "ngMaterial"]);
 
 angular.module("app").config(function ($mdThemingProvider, $mdIconProvider, $provide) {
-
     $mdIconProvider
         .icon("angular", "../assets/angular.svg", 128)
-
-
-    // $mdThemingProvider.generateThemesOnDemand(true);
-    // $provide.value('themeProvider', $mdThemingProvider);
 
     $mdThemingProvider.theme("blue")
         .primaryPalette("blue")
@@ -19,3 +14,12 @@ angular.module("app").config(function ($mdThemingProvider, $mdIconProvider, $pro
 
     $mdThemingProvider.alwaysWatchTheme(true);
 });
+
+//https://docs.angularjs.org/guide/production
+angular.module("app").config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
+    $compileProvider.commentDirectivesEnabled(false);
+    $compileProvider.cssClassDirectivesEnabled(false);
+}]);
+
+angular.bootstrap(document, ["app"]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,6 +1,4 @@
 function homeCtrl(){
-    var vm = this;
-
     this.$onInit = function(){
         console.info("init");
     }
